546fee8e5e71b32a6f4f9e94a34f364137c43906
valeriacano17 <valeria.cano@wipay.es>
2023-07-19 11:40:21 -0500

    rama para cambios nuevos del properties

--- src/main/java/com/wipay/payment/controllers/PaymentController.java
+++ src/main/java/com/wipay/payment/controllers/PaymentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+//COMMITVCS
 @RestController
 @RequestMapping ("/api/payment")
 public class PaymentController {
