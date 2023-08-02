b3947b3facbba968ff3c69fc75499fa51928b39e
MariaFernandaLopezWIPAY <mariafernanda.lopez@wipay.es>
2023-08-02 06:46:41 -0500

    Create SWAGGER and Update de POM for post method

--- src/main/java/com/wipay/payment/controllers/PaymentController.java
+++ src/main/java/com/wipay/payment/controllers/PaymentController.java
@@ -2,6 +2,7 @@ package com.wipay.payment.controllers;
 
 import com.wipay.payment.cybersource.model.DTOs.PaymentResponseDTO;
 import com.wipay.payment.cybersource.model.Payment;
+import com.wipay.payment.dynamodb.model.AuditPayment;
 import com.wipay.payment.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.server.ResponseStatusException;
-
 
 @RestController
 @RequestMapping ("/api/payment")
@@ -19,7 +24,18 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
     
-    @PostMapping
+
+    @PostMapping (value = "", consumes = "application/json")
+    @Operation (summary = "Guarda una transaccion",
+            description = "Este metedo guarda una transaccion realiza",
+            tags = {"Audit"})
+    @ApiResponses (value = {
+            @ApiResponse (responseCode = "200", description = "OK",
+                    content = {@Content (mediaType = "application/json",
+                            schema = @Schema (implementation = AuditPayment.class))}),
+            @ApiResponse (responseCode = "400", description = "Bad request", content = @Content),
+            @ApiResponse (responseCode = "404", description = " Page Not Found", content = @Content),
+            @ApiResponse (responseCode = "500", description = " Internal server error - view logs server", content = @Content)})
     public ResponseEntity<PaymentResponseDTO> createPayment(@RequestBody Payment payment) {
         try {
             var response = paymentService.createPaymentRequest(payment);
